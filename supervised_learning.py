55b655d9bb0c59d653843635248fbd2341669efd
ShinraTenseiRe <avidewan05@gmail.com>
2024-07-18 07:13:08 +0600

    epochs = 1

--- supervised_learning.py
+++ supervised_learning.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     parser.add_argument('--gamma', type=float, default=0.5)
     parser.add_argument('--momentum', type=float, default=0.9)
     parser.add_argument('--weight_decay', type=float, default=1e-4)
-    parser.add_argument('--epochs', default=100, type=int)
+    parser.add_argument('--epochs', default=1, type=int) # 100
     parser.add_argument('--step_size', default=10, type=int)
     parser.add_argument('--batch_size', default=128, type=int)
     parser.add_argument('--num_unlabeled_classes', default=5, type=int)
